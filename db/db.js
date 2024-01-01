26828cde47f3fc0846f421d5f50a646b082a9311
unknown <ahsanshakil.baust@gmail.com>
2024-01-01 10:43:35 +0600

    feat: connection & user model created

---
+++ db/db.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require("sequelize");
+
+const db = new Sequelize({
+  database: "media",
+  username: "root",
+  password: "Ahsan075@",
+  host: "localhost",
+  dialect: "mysql",
+  port: 5000,
+  logging: false,
+});
+
+try {
+  db.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+module.exports = db;
